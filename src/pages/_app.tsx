473e6861dd2b2180a06be473ad5993e1ba4a2483
Darius Vollmer <dariusv@outlook.de>
2023-01-22 00:15:19 +0100

    fixed _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,11 @@ import { AppProps, type AppType } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  return (
   <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
     <Component {...pageProps} />
   </main>
+  );
 }
 
 export default MyApp;
