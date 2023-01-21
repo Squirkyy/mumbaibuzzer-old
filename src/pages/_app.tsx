e1d1b0781643a80fae5ac1d0d2046b3ca0543339
Darius Vollmer <dariusv@outlook.de>
2023-01-21 21:06:04 +0100

    fixed _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
-import { type AppType } from "next/app";
+import { AppProps, type AppType } from "next/app";
 import "../styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return (
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        <Component {...pageProps} />
-      </main>
-  )
-};
+function MyApp({ Component, pageProps }: AppProps) {
+  <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+    <Component {...pageProps} />
+  </main>
+}
 
 export default MyApp;
