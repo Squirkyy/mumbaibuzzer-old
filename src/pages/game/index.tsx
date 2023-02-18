69f9eecd1e6a63f59cc6cd81137960c660e3ce67
Darius Vollmer <dariusv@outlook.de>
2023-02-18 23:42:56 +0100

    added redirect if game not in session

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,13 +1,23 @@
+import { doc } from "@firebase/firestore";
 import Router from "next/router";
 import { useEffect, useState } from "react";
+import { useDocument } from "react-firebase-hooks/firestore";
+import { db } from "../../utils/firebase";
 
 function Game() {
-  const [isInProgress, setIsInProgress] = useState(false);
+  const [value, loading, error] = useDocument(doc(db, "game", "info"));
+  const [isInProgress, setIsInProgress] = useState();
   useEffect(() => {
-    if (!isInProgress) {
+    console.log(value, loading, error);
+    if (!isInProgress && !loading) {
       Router.push("/game/player");
     }
   }, [isInProgress]);
+  useEffect(() => {
+    if (!loading) {
+      setIsInProgress(value?.data()?.isRunning);
+    }
+  }, [value, loading]);
   return <div>Game</div>;
 }
 
