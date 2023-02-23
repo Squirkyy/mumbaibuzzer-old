dee1358a403b4299a56e06b432871c5cb8e80476
Darius Vollmer <dariusv@outlook.de>
2023-02-23 20:37:37 +0100

    linting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,11 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-  <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-    <Component {...pageProps} />
-  </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+            <Component {...pageProps} />
+        </main>
+    );
 }
 
 export default MyApp;
