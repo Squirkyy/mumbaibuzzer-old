be4f8ae419491ada825ba082c959efd533ac9311
Darius Vollmer <dariusv@outlook.de>
2023-02-23 20:15:21 +0100

    added provisional list of player and teams

--- src/pages/game/commander.tsx
+++ src/pages/game/commander.tsx
@@ -1,5 +1,21 @@
+import useGameData from "../../utils/hooks";
+
 function Commander() {
-  return <div>Commander</div>;
+    const [teams, players, loading] = useGameData();
+    const playerListItem = players
+        ? players.map((player) => <li key={player}>{player}</li>)
+        : null;
+    const teamListItem = teams
+        ? teams.map((team) => <li key={team}>{team.name}</li>)
+        : null;
+    return (
+        <>
+            <h1>Commander</h1>
+            {loading ? <div>loading</div> : <ul>{playerListItem}</ul>}
+            <br />
+            {loading ? <div>loading</div> : <ul>{teamListItem}</ul>}
+        </>
+    );
 }
 
 export default Commander;
