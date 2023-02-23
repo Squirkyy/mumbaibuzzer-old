b2154132898ec19e0bdeca54eaf56cfc01f76a9f
Darius Vollmer <dariusv@outlook.de>
2023-02-23 20:13:46 +0100

    linting

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -5,20 +5,20 @@ import { useDocument } from "react-firebase-hooks/firestore";
 import { db } from "../../utils/firebase";
 
 function Game() {
-  const [value, loading, error] = useDocument(doc(db, "game", "info"));
-  const [isInProgress, setIsInProgress] = useState();
-  useEffect(() => {
-    console.log(value, loading, error);
-    if (!isInProgress && !loading) {
-      Router.push("/game/player");
-    }
-  }, [isInProgress]);
-  useEffect(() => {
-    if (!loading) {
-      setIsInProgress(value?.data()?.isRunning);
-    }
-  }, [value, loading]);
-  return <div>Game</div>;
+    const [value, loading, error] = useDocument(doc(db, "game", "info"));
+    const [isInProgress, setIsInProgress] = useState();
+    useEffect(() => {
+        console.log(value, loading, error);
+        if (!isInProgress && !loading) {
+            Router.push("/game/player");
+        }
+    }, [isInProgress]);
+    useEffect(() => {
+        if (!loading) {
+            setIsInProgress(value?.data()?.isRunning);
+        }
+    }, [value, loading]);
+    return <div>Game</div>;
 }
 
 export default Game;
--- src/pages/game/player.tsx
+++ src/pages/game/player.tsx
@@ -5,20 +5,20 @@ import { useDocument } from "react-firebase-hooks/firestore";
 import { db } from "../../utils/firebase";
 
 function Player() {
-  const [value, loading, error] = useDocument(doc(db, "game", "info"));
-  const [isInProgress, setIsInProgress] = useState();
-  useEffect(() => {
-    console.log(value, loading, error);
-    if (isInProgress && !loading) {
-      Router.push("/game");
-    }
-  }, [isInProgress]);
-  useEffect(() => {
-    if (!loading) {
-      setIsInProgress(value?.data()?.isRunning);
-    }
-  }, [value, loading]);
-  return <div>Player</div>;
+    const [value, loading, error] = useDocument(doc(db, "game", "info"));
+    const [isInProgress, setIsInProgress] = useState();
+    useEffect(() => {
+        console.log(value, loading, error);
+        if (isInProgress && !loading) {
+            Router.push("/game");
+        }
+    }, [isInProgress]);
+    useEffect(() => {
+        if (!loading) {
+            setIsInProgress(value?.data()?.isRunning);
+        }
+    }, [value, loading]);
+    return <div>Player</div>;
 }
 
 export default Player;
