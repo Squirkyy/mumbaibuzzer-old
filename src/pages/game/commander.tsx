32bc3ed56e67a3b5df8a32e4054a9f43904f4fda
Darius Vollmer <dariusv@outlook.de>
2023-03-09 21:46:23 +0100

    removed errors and reworked it

--- src/pages/game/commander.tsx
+++ src/pages/game/commander.tsx
@@ -1,97 +1,51 @@
 import { useGameData } from "../../utils/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MultiSelect } from "@mantine/core";
 import type { Player, Team } from "../../utils/hooks";
 
 function Commander() {
     const [teams, players, loading] = useGameData();
-    const [teamSelections, setTeamSelections] = useState<{
-        [teamName: string]: Player[];
-    }>({});
+    const [teamSelections, setTeamSelections] = useState<Team[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    if (loading) {
-        return <div>Loading....</div>;
-    }
+    useEffect(() => {
+        if (!loading) {
+            setTeamSelections(teams);
+            setIsLoading(false);
+        }
+    }, [loading]);
 
-    // Filter out the "no_team" and create a MultiSelect component for each team
-    const teamSelects = teams
-        .filter((team) => team.name !== "no_team")
-        .map((team) => (
-            <div key={team.name}>
-                <h2>{team.name}</h2>
-                <MultiSelect
-                    data={players.filter(
-                        (player) =>
-                            teamSelections[team.name]?.includes(player) ?? true
-                    )}
-                    label="Select Players"
-                    placeholder="Select players"
-                    searchable
-                    nothingFound="No players found"
-                    multiple
-                    onChange={(selectedPlayers) => {
-                        // Remove selected players from no_team selection
-                        setTeamSelections((prevSelections) => {
-                            const noTeamSelection =
-                                prevSelections.no_team || [];
-                            const newNoTeamSelection = noTeamSelection.filter(
-                                (player) => !selectedPlayers.includes(player)
-                            );
-                            return {
-                                ...prevSelections,
-                                no_team: newNoTeamSelection,
-                                [team.name]: selectedPlayers,
-                            };
-                        });
-                    }}
-                />
-            </div>
-        ));
+    const unselectedPlayers = teamSelections
+        .filter((team) => team.name === "no_team")
+        .flatMap((team) =>
+            team.player.map((person) => ({
+                label: person,
+                value: person,
+            }))
+        );
 
-    // Create the MultiSelect component for the "no_team" selection
-    const noTeamSelect = (
-        <div>
-            <h2>No Team</h2>
-            <MultiSelect
-                data={
-                    players.filter(
-                        (player) =>
-                            !Object.values(teamSelections)
-                                .flat()
-                                .includes(player)
-                    ) as string[]
-                }
-                label="Select Players"
-                placeholder="Select players"
-                searchable
-                nothingFound="No players found"
-                multiple
-                onChange={(selectedPlayers) => {
-                    const newSelections = { ...teamSelections };
-                    Object.keys(newSelections).forEach((teamName) => {
-                        newSelections[teamName] = newSelections[teamName]
-                            ? newSelections[teamName].filter(
-                                  (player) => !selectedPlayers.includes(player)
-                              )
-                            : [];
-                    });
-                    // Ensure no_team selection is always an array
-                    newSelections.no_team = selectedPlayers ?? [];
-                    setTeamSelections(newSelections);
-                }}
-            />
-        </div>
-    );
+    const TeamSelect =
+        !isLoading &&
+        teamSelections //.filter((team) => team.name !== "no_team")
+            .map((team) => (
+                <div key={team.name}>
+                    <MultiSelect
+                        data={unselectedPlayers}
+                        label={team.name}
+                        value={team.player}
+                        placeholder="Pick all that you like"
+                    />
+                </div>
+            ));
 
     return (
         <>
             <h1>Commander</h1>
-            {teamSelects}
-            {noTeamSelect}
+            {isLoading ? <div>Loading....</div> : TeamSelect}
             <button
                 onClick={() => {
                     // Save team selections to db
-                    console.log(teamSelections);
+                    console.log(unselectedPlayers);
                 }}
             >
                 Save
