1a32aad340625e965f15780a9fea01e1d40e20c8
Darius Vollmer <dariusv@outlook.de>
2023-06-02 16:25:01 +0200

    added remove all users button

--- src/pages/game/commander.tsx
+++ src/pages/game/commander.tsx
@@ -1,16 +1,38 @@
+import { Button } from "@mantine/core";
 import { useGameData } from "../../utils/hooks";
+import { collection, deleteDoc, getDocs } from "firebase/firestore";
+import { db } from "../../utils/firebase";
 
 function Commander() {
     const [players, loading] = useGameData();
     const playerList = players.map((user) => (
         <li key={user.Name}>{user.Name}</li>
     ));
+
+    async function removeAllUser() {
+        const colRef = collection(db, "player");
+        const snapshop = await getDocs(colRef);
+
+        snapshop.forEach((doc) => {
+            deleteDoc(doc.ref);
+        });
+    }
+
     if (loading) {
         return <div>Loading Players...</div>;
     } else {
         return (
             <>
-                <ul>{playerList}</ul>
+                {players.some((player) => player.Name !== null) ? (
+                    <>
+                        <ul>{playerList}</ul>
+                        <Button className="btn" onClick={() => removeAllUser()}>
+                            Remove all Users
+                        </Button>
+                    </>
+                ) : (
+                    <h1>No Players here Yet.....</h1>
+                )}
             </>
         );
     }
