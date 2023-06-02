967f0c47f00f43e2940140f4814ac62b28c05536
Darius Vollmer <dariusv@outlook.de>
2023-06-02 15:12:22 +0200

    removed team picker

--- src/pages/game/commander.tsx
+++ src/pages/game/commander.tsx
@@ -1,57 +1,19 @@
 import { useGameData } from "../../utils/hooks";
-import { useEffect, useState } from "react";
-import { MultiSelect } from "@mantine/core";
-import type { Player, Team } from "../../utils/hooks";
 
 function Commander() {
-    const [teams, players, loading] = useGameData();
-    const [teamSelections, setTeamSelections] = useState<Team[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        if (!loading) {
-            setTeamSelections(teams);
-            setIsLoading(false);
-        }
-    }, [loading]);
-
-    const unselectedPlayers = teamSelections
-        .filter((team) => team.name === "no_team")
-        .flatMap((team) =>
-            team.player.map((person) => ({
-                label: person,
-                value: person,
-            }))
+    const [players, loading] = useGameData();
+    const playerList = players.map((user) => (
+        <li key={user.Name}>{user.Name}</li>
+    ));
+    if (loading) {
+        return <div>Loading Players...</div>;
+    } else {
+        return (
+            <>
+                <ul>{playerList}</ul>
+            </>
         );
-
-    const TeamSelect =
-        !isLoading &&
-        teamSelections //.filter((team) => team.name !== "no_team")
-            .map((team) => (
-                <div key={team.name}>
-                    <MultiSelect
-                        data={unselectedPlayers}
-                        label={team.name}
-                        value={team.player}
-                        placeholder="Pick all that you like"
-                    />
-                </div>
-            ));
-
-    return (
-        <>
-            <h1>Commander</h1>
-            {isLoading ? <div>Loading....</div> : TeamSelect}
-            <button
-                onClick={() => {
-                    // Save team selections to db
-                    console.log(unselectedPlayers);
-                }}
-            >
-                Save
-            </button>
-        </>
-    );
+    }
 }
 
 export default Commander;
