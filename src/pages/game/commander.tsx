d51096a22737b06c092c21b09091ef486a4e6769
Darius Vollmer <dariusv@outlook.de>
2023-06-04 17:42:17 +0200

    added working commander view

--- src/pages/game/commander.tsx
+++ src/pages/game/commander.tsx
@@ -1,10 +1,16 @@
-import { Button } from "@mantine/core";
-import { useGameData } from "../../utils/hooks";
-import { collection, deleteDoc, getDocs } from "firebase/firestore";
+import { useGameData, useGameInfo } from "../../utils/hooks";
+import {
+    collection,
+    deleteDoc,
+    doc,
+    getDocs,
+    updateDoc,
+} from "firebase/firestore";
 import { db } from "../../utils/firebase";
 
 function Commander() {
-    const [players, loading] = useGameData();
+    const [players, loading, buzzed] = useGameData();
+    const [isInProgress, gameinfoLoading] = useGameInfo();
     const playerList = players.map((user) => (
         <li key={user.Name}>{user.Name}</li>
     ));
@@ -18,9 +24,27 @@ function Commander() {
         });
     }
 
-    if (loading) {
-        return <div>Loading Players...</div>;
-    } else {
+    async function removeAllBuzzed() {
+        const colRef = collection(db, "game/info/buzzed");
+        const snapshop = await getDocs(colRef);
+
+        snapshop.forEach((doc) => {
+            deleteDoc(doc.ref);
+        });
+    }
+
+    const toggleState = async () => {
+        if (isInProgress) {
+            removeAllUser();
+            removeAllBuzzed();
+        }
+        const docRef = doc(db, "game", "info");
+        await updateDoc(docRef, {
+            isRunning: !isInProgress,
+        });
+    };
+
+    const setupStage = (): JSX.Element => {
         return (
             <>
                 {players.some((player) => player.Name !== null) ? (
@@ -33,6 +57,38 @@ function Commander() {
                 ) : (
                     <h1>No Players here Yet.....</h1>
                 )}
+            </>
+        );
+    };
+
+    const gameStage = () => {
+        return (
+            <>
+                <h1>Game View</h1>
+                <ol>
+                    {buzzed.map((player) => (
+                        <li key={player.timestamp.toString()}>{player.Name}</li>
+                    ))}
+                </ol>
+                <button onClick={() => removeAllBuzzed()}>
+                    Remove the buzzed
+                </button>
+            </>
+        );
+    };
+
+    if (loading || gameinfoLoading) {
+        return <div>Loading Players...</div>;
+    } else {
+        return (
+            <>
+                {!isInProgress ? setupStage() : gameStage()}
+                <button
+                    className="btn-accent btn"
+                    onClick={() => toggleState()}
+                >
+                    Toggle Game in Progress
+                </button>
             </>
         );
     }
