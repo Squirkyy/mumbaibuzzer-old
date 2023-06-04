d3a8391d321a8e8a4fa001d941808cb7ed172dcb
Darius Vollmer <dariusv@outlook.de>
2023-06-04 17:42:29 +0200

    gameview is working as proposed

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,19 +1,51 @@
 import Router from "next/router";
-import { useEffect } from "react";
-import { useGameInfo } from "../../utils/hooks";
+import { useEffect, useState } from "react";
+import { useGameData, useGameInfo } from "../../utils/hooks";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { db } from "../../utils/firebase";
 
 function Game() {
+    const [buttonEnabled, setButtonEnabled] = useState(true);
+    const [, loading, buzzed] = useGameData();
     const [isInProgress, progressLoading] = useGameInfo();
+    const [user, setUser] = useState<string | null>(null);
     useEffect(() => {
         if (!isInProgress && !progressLoading) {
             Router.push("/game/player");
         }
-    }, [isInProgress]);
+    }, [isInProgress, progressLoading]);
 
-    const reportBuzz = () => {};
+    useEffect(() => {
+        setUser(localStorage.getItem("username"));
+    }, []);
+
+    useEffect(() => {
+        if (!loading && buzzed.length == 0) {
+            setButtonEnabled(true);
+        }
+    }, [buzzed, loading]);
+
+    const reportBuzz = async () => {
+        const collRef = collection(db, "game/info/buzzed");
+        await addDoc(collRef, {
+            name: user,
+            timestamp: serverTimestamp(),
+        });
+        setButtonEnabled(false);
+    };
+    if (user == null) {
+        return <h1>You have no permissions to be here.</h1>;
+    }
     return (
         <>
-            <button>Buzz</button>
+            You are playing as {user}.
+            <button
+                disabled={!buttonEnabled}
+                className="btn-primary btn"
+                onClick={() => reportBuzz()}
+            >
+                Buzz
+            </button>
         </>
     );
 }
