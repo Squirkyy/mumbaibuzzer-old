ce7381d1505271b7df2840c55c7a723ffd1cbac6
Darius Vollmer <dariusv@outlook.de>
2023-06-11 15:18:33 +0200

    removed unnessesary TODO

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,11 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAtQEMBn1khDHE_fL_h5XGHeWl4AOPixX8",
   authDomain: "mumbaibuzzer.firebaseapp.com",
