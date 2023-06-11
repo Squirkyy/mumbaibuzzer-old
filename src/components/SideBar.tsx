86826def4ff203d922cc3447339e79c0f6db24d6
Darius Vollmer <dariusv@outlook.de>
2023-06-11 15:19:36 +0200

    conditional render on the kick all players button

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,9 @@
 import { collection, getDocs, deleteDoc } from "firebase/firestore";
 import { db } from "../utils/firebase";
 import PlayerList from "./PlayerList";
+import { useRouter } from "next/router";
 
+//make countercheck on user side with token in localstorage
 export async function removeAllUser() {
     const colRef = collection(db, "player");
     const snapshop = await getDocs(colRef);
@@ -11,16 +13,23 @@ export async function removeAllUser() {
     });
 }
 
-
 function SideBar() {
-    return ( 
-        <ul className="menu bg-slate-900  flex flex-col basis-1/12 ml-5 items-center">
-                <button className="btn btn-accent mt-5 mb-4" onClick={() => removeAllUser()}>Kick all players</button>
-                <div className="divider mx-4 my-0"/>
-                <h1 className="text-xl text-center text-gray-200 my-2">Players</h1>
-                <PlayerList />
+    const router = useRouter();
+    return (
+        <ul className="menu ml-5  flex basis-1/12 flex-col items-center bg-slate-900">
+            {router.pathname == "/game/commander" && (
+                <button
+                    className="btn-accent btn mb-4 mt-5"
+                    onClick={() => removeAllUser()}
+                >
+                    Kick all players
+                </button>
+            )}
+            <div className="divider mx-4 my-0" />
+            <h1 className="my-2 text-center text-xl text-gray-200">Players</h1>
+            <PlayerList />
         </ul>
-    )
+    );
 }
 
 export default SideBar;
