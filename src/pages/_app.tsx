34b0113ec9e8bc3eee15836ca13a94d29f044893
Darius Vollmer <dariusv@outlook.de>
2023-06-11 14:59:10 +0200

    added conditional render for ToggleGame

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,20 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import SideBar from "../components/SideBar";
 import ToggleGame from "../components/ToggleGame";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
     return (
         <>
-            <main data-theme="night" className="flex flex-row min-h-screen bg-gradient-to-br from-[#2e026d] to-[#15162c]">
-                <div className="flex flex-col basis-1/12 m-2">
-                    <ToggleGame />
+            <main
+                data-theme="night"
+                className="flex min-h-screen flex-row bg-gradient-to-br from-[#2e026d] to-[#15162c]"
+            >
+                <div className="m-2 flex basis-1/12 flex-col">
+                    {router.pathname == "/game/commander" && <ToggleGame />}
                 </div>
-                <div className="flex flex-col basis-10/12 items-center justify-center">
+                <div className="flex basis-10/12 flex-col items-center justify-center">
                     <Component {...pageProps} />
                 </div>
                 <SideBar />
