e3a2a1339942ae1bd7ef57b37d4d6a11df3b287f
Darius Vollmer <dariusv@outlook.de>
2023-06-24 01:13:09 +0200

    fuck you, grandma

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@ function HomePage() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center">
             <h1>MF Brainlag</h1>
-            <h2>It's good and stuff</h2>
+            <h2>Its good and stuff</h2>
             <h3>You can be here, without being logged in</h3>
         </div>
     );
